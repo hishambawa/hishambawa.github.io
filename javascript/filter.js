c913ae0fcf9922f9888237aff15946c8b3f7fff1
Hisham Bawa <hishambawa@icloud.com>
2021-06-07 23:42:53 +0530

    Added Products, Functional Filter, Filter Animations

--- javascript/filter.js
+++ javascript/filter.js
@@ -1,22 +1,42 @@
 function filterItems(item){
     let label = document.getElementById("filter-label");
+    let filter = document.getElementById("filter");
 
     switch(item){
 
         case "all":
             label.innerText = "All";
+            loadProducts("all");
+            filter.style.transform = "translateY(-1000px)";
+            setTimeout(function(){filter.style.display = "none";}, 400);
             break;
+
         case "t-shirts":
             label.innerText = "T-Shirts";
+            loadProducts("tshirts");
+            filter.style.transform = "translateY(-1000px)";
+            setTimeout(function(){filter.style.display = "none";}, 400);
             break;
+
         case "shorts":
             label.innerText = "Shorts";
+            loadProducts("shorts");
+            filter.style.transform = "translateY(-1000px)";
+            setTimeout(function(){filter.style.display = "none";}, 400);
             break;
+
         case "shoes":
             label.innerText = "Shoes";
+            loadProducts("shoes");
+            filter.style.transform = "translateY(-1000px)";
+            setTimeout(function(){filter.style.display = "none";}, 400);
             break;
+            
         case "accessories":
             label.innerText = "Accessories";
+            loadProducts("accessories");
+            filter.style.transform = "translateY(-1000px)";
+            setTimeout(function(){filter.style.display = "none";}, 400);
             break;
     }
 }
--- javascript/script.js
+++ javascript/script.js
@@ -15,6 +15,8 @@ function filter(){
 
     let filter = document.getElementById("filter");
 
+    filter.style.transform = "translateX(0)";
+
     if(filter.style.display == "block"){
         filter.style.display = "none";
     }
@@ -36,3 +38,56 @@ function closeCart(){
     document.getElementById("cart-content").style.top = "-100vh";
 
 }
+
+function loadProducts(catagory){
+
+    let container = document.getElementById("content");
+
+    container.innerHTML = "";
+
+    for(var i in items){
+
+        if(catagory == "all" || items[i].catagory == catagory){
+            container.innerHTML += 
+            `
+            <div class="item" id=" `+ items[i].code +`">
+                <img src="https://dummyimage.com/600x400/000/fff" class="item-image">
+                <p class="item-name">`+ items[i].name +`</p>
+                <p class="item-price">$`+ items[i].price +`</p>
+            </div>
+            `;
+
+        }
+    }
+}
+
+let items =
+[
+    {
+        code:"001",
+        name:"Item Tshirt",
+        price:"25",
+        catagory:"tshirts"
+    },
+
+    {
+        code:"002",
+        name:"Item Shorts",
+        price:"40",
+        catagory:"shorts"
+    },
+
+    {
+        code:"003",
+        name:"Item Shoes",
+        price:"15",
+        catagory:"shoes"
+    },
+
+    {
+        code:"004",
+        name:"Item Accessories",
+        price:"50",
+        catagory:"accessories"
+    },
+]
