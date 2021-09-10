86e4b1f9a0779a56d5bae206759b76e53c6a5833
Hisham Bawa <hishambawa@icloud.com>
2021-09-10 21:34:56 +0530

    Layout 1 Final

---
+++ assets/js/common.js
@@ -0,0 +1,44 @@
+// Scroll to a specific div smoothly. Similar to scroll-behaviour: smooth, and works on all browsers.
+function smoothScroll(pos, time){
+    var currentPos = window.pageYOffset;
+    var start = null;
+    if(time == null) time = 500;
+    pos = +pos, time = +time;
+    window.requestAnimationFrame(function step(currentTime) {
+        start = !start ? currentTime : start;
+        var progress = currentTime - start;
+        if (currentPos < pos) {
+            window.scrollTo(0, ((pos - currentPos) * progress / time) + currentPos);
+        } else {
+            window.scrollTo(0, currentPos - ((currentPos - pos) * progress / time));
+        }
+        if (progress < time) {
+            window.requestAnimationFrame(step);
+        } else {
+            window.scrollTo(0, pos);
+        }
+    });
+}
+
+// Show / Hide the "Back to Top" icon on scroll
+window.addEventListener("scroll", function(event){
+    if(window.scrollY > 300){
+        document.getElementById("top").style.display = "block"; // Show the back to top button
+    }
+    else{
+        document.getElementById("top").style.display = "none";  // Hide the back to top button
+    }
+});
+
+// Show not supported dialog on IE
+window.addEventListener("load", function(){
+    var isIE = !!navigator.userAgent.match(/Trident/g) || !!navigator.userAgent.match(/MSIE/g);
+    if(isIE){
+        document.getElementById("incompatible").style.display = "flex";
+
+        document.getElementById("chrome").href = "microsoft-edge:" + document.URL;
+        document.getElementById("safari").href = "microsoft-edge:" + document.URL;
+        document.getElementById("edge").href = "microsoft-edge:" + document.URL;
+        document.getElementById("firefox").href = "microsoft-edge:" + document.URL;
+    } 
+});
